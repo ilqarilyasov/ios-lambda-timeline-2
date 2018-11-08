e2520aba271934cdb7642e20648d7f7bfd24100f
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-11-07 21:18:20 -0600

    Audio recording and playing completed

--- LambdaTimeline/Resources/AppDelegate.swift
+++ LambdaTimeline/Resources/AppDelegate.swift
@@ -28,6 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             window?.makeKeyAndVisible()
         }
         
+        AddCommentViewController.requestRecordPermission()
         
         return true
     }
--- LambdaTimeline/View Controllers/AddCommentViewController.swift
+++ LambdaTimeline/View Controllers/AddCommentViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Firebase
 import AVFoundation
 
-class AddCommentViewController: UIViewController {
+class AddCommentViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,15 +37,102 @@ class AddCommentViewController: UIViewController {
     
     
     @IBAction func record(_ sender: Any) {
+        defer { updateButton() }
         
+        guard !isRecording else {
+            recorder?.stop()
+            return
+        }
+        
+        do {
+            let format = AVAudioFormat(standardFormatWithSampleRate: 44100.0, channels: 2)!
+            recorder = try AVAudioRecorder(url: newRecordingURL(), format: format)
+            recorder?.delegate = self
+            recorder?.record()
+        } catch {
+            NSLog("Unable to start recording: \(error.localizedDescription)")
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        recordingURL = recorder.url
+        self.recorder = nil
+        updateButton()
     }
     
     @IBAction func play(_ sender: Any) {
+        defer { updateButton() }
+        guard let audioURL = recordingURL else { return }
         
+        guard !isPlaying else {
+            player?.pause()
+            return
+        }
+        
+        if player != nil && !isPlaying {
+            player?.play()
+            return
+        }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: audioURL)
+            player?.delegate = self
+            player?.play()
+        } catch {
+            NSLog("Unable to play audio: \(error.localizedDescription)")
+        }
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        self.player = nil
+        updateButton()
     }
     
     @IBAction func send(_ sender: Any) {
         
+    }
+    
+    class func requestRecordPermission() {
+        let session = AVAudioSession.sharedInstance()
+        
+        session.requestRecordPermission { (granted) in
+            guard granted else {
+                NSLog("Please give the application permission to record in Settings")
+                return
+            }
+            
+            do {
+                try session.setCategory(.playAndRecord, mode: .default, options: [])
+                try session.setActive(true, options: [])
+            } catch {
+                NSLog("Error setting AVAudioSession: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    private func updateButton() {
+        let recording = isRecording ? "Stop" : "Record"
+        recordButton.setTitle(recording, for: .normal)
+        
+        let playing = isPlaying ? "Stop" : "Play"
+        playButton.setTitle(playing, for: .normal)
+    }
+    
+    private func newRecordingURL() -> URL {
+        let dir = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        return dir.appendingPathComponent(UUID().uuidString).appendingPathExtension("caf")
+    }
+    
+    private var recorder: AVAudioRecorder?
+    private var player: AVAudioPlayer?
+    private var recordingURL: URL?
+    
+    private var isRecording: Bool {
+        return recorder?.isRecording ?? false
+    }
+    
+    private var isPlaying: Bool {
+        return player?.isPlaying ?? false
     }
     
     @IBOutlet weak var commentTypeSegmentedControl: UISegmentedControl!
